326e63c5e7f4c7f9295492bc5dfff7e96bd0f787
Swarnadeep Ghosh <swarna.lyf@gmail.com>
2021-02-28 18:13:41 +0530

    added "Please enter a value" feature

--- script.js
+++ script.js
@@ -2,16 +2,16 @@ function validateForm() {
     var u = document.forms["myForm"]["userId"].value;
     var p = document.forms["myForm"]["password"].value;
     if (u == "" || p == "") {
-        alert("User ID and Password must not be EMPTY");
+        alert("User ID or Password must not be EMPTY");
         return false;
     }
     else if (u == "admin123" || p == "adminpassword") {
-        alert("Login Successful");
+        alert("Login SUCCESSFUL");
         return false;
     }
 
     else {
-        alert("Please enter correct credentials");
+        alert("Please enter CORRECT credentials");
         return false;
     }
 
@@ -20,23 +20,23 @@ function validateForm() {
 function validateUserId() {
     var u = document.getElementById("userId").value;
     var pattern = /^[a-zA-Z0-9\-]+$/;
-
-    /*
+    
     //alert(u);    
     //var pattern = "[a-zA-Z0-9]+";
     if(u == ""){
-        console.log("please enter userid")
-        document.getElementById("user").textContent = "Please enter User Id";
-        document.getElementById("user").style.display = "block";
-    }
-    */
-   
-    if(u.match(pattern)){
-        //console.log("matched")
+        //console.log("please enter userid")
+        document.getElementById("value1").style.display = "block";
         document.getElementById("user").style.display = "none";
     }
-    else{
+    
+    else if (u.match(pattern)) {
+        //console.log("matched")
+        document.getElementById("user").style.display = "none";
+        document.getElementById("value1").style.display = "none";
+    }
+    else {
         document.getElementById("user").style.display = "block";
+        document.getElementById("value1").style.display = "none";
         //console.log("not matched")
     }
 }
@@ -44,22 +44,21 @@ function validateUserId() {
 function validatePassword() {
     var p = document.getElementById("password").value;
     
-    /*
     //alert(p);  
     if(p == ""){
-        console.log("please enter password")
-        document.getElementById("pass").textContent = "Please enter password";
-        document.getElementById("pass").style.display = "block";
-    }
-    */ 
-
-    if(p.length < 8){
-        console.log("password less than 8")
-        document.getElementById("pass").style.display = "block";
-    }
-    else{
+        //console.log("please enter password")
+        document.getElementById("value2").style.display = "block";
         document.getElementById("pass").style.display = "none";
-        console.log("password is ok")
+    }
+    else if (p.length < 8) {
+        //console.log("password less than 8")
+        document.getElementById("pass").style.display = "block";
+        document.getElementById("value2").style.display = "none";
+    }
+    else {
+        document.getElementById("pass").style.display = "none";
+        document.getElementById("value2").style.display = "none";
+        //console.log("password is ok")
     }
 
 }
