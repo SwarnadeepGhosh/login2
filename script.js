d780d7870554f2b76fcd4a633aed8d71e1e6c507
Swarnadeep Ghosh <swarna.lyf@gmail.com>
2021-03-06 19:35:11 +0530

    Added JSON print functionality

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-function validateForm() {
+
+function validateLogin() {
     var u = document.forms["myForm"]["userId"].value;
     var p = document.forms["myForm"]["password"].value;
     if (u == "" || p == "") {
@@ -19,15 +20,15 @@ function validateForm() {
 function validateUserId() {
     var u = document.getElementById("userId").value;
     var pattern = /^[a-zA-Z0-9\-]+$/;
-    
+
     //alert(u);    
     //var pattern = "[a-zA-Z0-9]+";
-    if(u == ""){
+    if (u == "") {
         //console.log("please enter userid")
         document.getElementById("value1").style.display = "block";
         document.getElementById("user").style.display = "none";
     }
-    
+
     else if (u.match(pattern)) {
         //console.log("matched")
         document.getElementById("user").style.display = "none";
@@ -42,9 +43,9 @@ function validateUserId() {
 
 function validatePassword() {
     var p = document.getElementById("password").value;
-    
+
     //alert(p);  
-    if(p == ""){
+    if (p == "") {
         //console.log("please enter password")
         document.getElementById("value2").style.display = "block";
         document.getElementById("pass").style.display = "none";
@@ -68,7 +69,41 @@ function confirmPassword() {
     if (p1 != p2) {
         document.getElementById("confirmPassword").style.display = "block";
     }
-    else{
+    else {
         document.getElementById("confirmPassword").style.display = "none";
     }
+}
+
+var users = [];
+
+function result(event) {
+    event.preventDefault();
+
+    var user = {
+        name: document.getElementById('name').value,
+        age: document.getElementById('age').value,
+        company: document.getElementById('company').value,
+        gender: '',
+        phone: document.getElementById('phone').value,
+        userId: document.getElementById('userId').value,
+        password: document.getElementById('password').value
+    }
+    //gender: document.getElementsByClassName("form-check-label").value,
+    
+    if(document.getElementById('male').checked){
+        user.gender = document.getElementById('male').value;
+    }
+    else if(document.getElementById('female').checked){
+        user.gender = document.getElementById('female').value;
+    }
+    else if(document.getElementById('notToSay').checked){
+        user.gender = document.getElementById('notToSay').value;
+    }
+
+
+    users.push(user);
+    document.getElementById("input_values").innerHTML = JSON.stringify(users);
+    console.log(users)
+    //alert(users);
+
 }
